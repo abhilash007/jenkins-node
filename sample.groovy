0d22dd5ea584217fdd952c5e1b0573ecf71b47ed
abhilashkv <ranjuabhi007@gmail.com>
2026-03-20 01:57:42 +0530

    Add environment variable loading from YAML configuration

--- sample.groovy
+++ sample.groovy
@@ -4,15 +4,29 @@ pipeline {
     tools {
         nodejs 'node-18'
     }
-    environment {
-        DB_USER_NAME = 'TEST123'
-    }
+   
 
     stages {
 
         stage('Clone Repo') {
             steps {
                 git branch: 'main', url: 'https://github.com/abhilash007/jenkins-node.git'
+            }
+        }
+
+        stage('Load Env from YAML') {
+            steps {
+                script {
+                    def config = readYaml file: 'config/stage-variables.yml'
+
+                    def envVars = config['dev']['EnvVariables']
+
+                    env.STAGE = envVars.STAGE
+                    env.DB_USER_NAME = envVars.DB_USER_NAME
+                    env.DB_PASSWORD = envVars.DB_PASSWORD
+
+                    echo "Loaded ENV for dev"
+                }
             }
         }
 
